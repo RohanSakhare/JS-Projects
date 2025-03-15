1d592b0a0533071228d8324268d6ce8a12844ed8
RohanSakhare <rohanpatilsakhare@gmail.com>
2025-03-15 14:34:56 +0530

    task manager added

---
+++ Auto Typer/script.js
@@ -0,0 +1,11 @@
+
+const typed = new Typed('.auto-type', {
+    strings: ['Developer', 'Designer', 'Student', 'Freelancerasd'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+    backDelay: 1000,
+    cursorChar: "!",
+    smartBackspace: true,
+})  
+
